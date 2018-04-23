5058e90d68c67728ed7fa25aab3d2e4a6598010f
Nytheneals <nythes@gmail.com>
2018-04-22 23:14:19 -0400

    [Initial Redux  setup]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 //********************MAIN COMPONENT********************//
 
@@ -12,3 +12,43 @@ const App = () => {
 };
 
 export default App;
+
+// DEFAULT STATE
+
+const defaultState = {
+  welcome: "Hi",
+  otherState: "OtherStuff",
+  counter: "Numerical"
+};
+
+// REDUCER
+const greetingsReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "GREET_ME":
+      return {
+        ...state,
+        welcome: `Hello ${action.payload}`
+      };
+    case "GREET_WORLD":
+      return {
+        ...state,
+        welcome: "Hello world"
+      };
+
+    default:
+      return state;
+  }
+};
+
+// STORE
+
+const store = createStore(greetingsReducer);
+
+// ACTION
+
+store.dispatch({
+  type: "GREET_ME",
+  payload: "Natheneals"
+});
+
+console.log(store.getState());
